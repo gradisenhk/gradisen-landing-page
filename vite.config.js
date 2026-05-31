538038c3028d5c38906ae627000842e776a18d14
felixfongfsc <gradisenhk@gmail.com>
2026-06-01 00:15:07 +0800

    feat: testing for path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gradisen-landing-page/',
+  base: '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
